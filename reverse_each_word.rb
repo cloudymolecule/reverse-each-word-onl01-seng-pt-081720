fa27d13685acd7550369ad815842b22c6378dc5d
Dario Carlino <baires82@gmail.com>
2020-08-21 03:18:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(string)
+  result = ""
   array = string.split(" ")
   array.map do |twist|
     twist.reverse
+    if string[-1]
+      result << "#{twist}"
+    else
+      result << "#{twist} "
+    end
   end
 end
