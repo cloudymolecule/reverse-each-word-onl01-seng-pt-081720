5ccabd902e75aa1e947607402989338295555e44
Dario Carlino <baires82@gmail.com>
2020-08-21 03:19:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,6 @@
 def reverse_each_word(string)
-  result = ""
   array = string.split(" ")
   array.map do |twist|
     twist.reverse
-    if string[-1]
-      result << "#{twist}"
-    else
-      result << "#{twist} "
-    end
   end
 end
